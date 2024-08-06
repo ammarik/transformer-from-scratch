35ee99ffb4f0b499457833f2686b4145bfc33748
Marek Marik <marek.marik@ibm.com>
2024-08-06 17:25:59 +0200

    Encoder

--- model.py
+++ model.py
@@ -148,4 +148,13 @@ class EncoderBlock(nn.Module):
         return x
 
 
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
 
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
